acf4b7d8a812b779102d067cc7f7485e1cbe138b
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2019-10-03 05:27:45 +0300

    Use splice method to modify array

--- src/parents.js
+++ src/parents.js
@@ -19,42 +19,38 @@ export function stopCapturingParents() {
 }
 
 export function maybeCaptureParent(p) {
-  if (child !== null) {
-    const frame = parentsStack[parentsStack.length - 1];
-    if (frame.parents[frame.offset] === p) {
-      // nothing to do, just skip over
-      frame.offset++;
-    } else {
-      // look for this parent elsewhere
-      const idx = frame.parents.indexOf(p);
-      if (idx === -1) {
-        // not seen this parent yet, add it in the correct place
-        // and push the one currently there to the end (likely that we'll be
-        // getting rid of it)
-        // sneaky hack for doing captureDereferences
-        if (child !== void 0) {
-          addToArray(p._activeChildren, child);
-        }
-        if (frame.offset === frame.parents.length) {
-          frame.parents.push(p);
-        } else {
-          frame.parents.push(frame.parents[frame.offset]);
-          frame.parents[frame.offset] = p;
-        }
-        frame.offset++;
-      } else {
-        if (idx > frame.offset) {
-          // seen this parent after current point in array, so swap positions
-          // with current point's parent
-          const tmp = frame.parents[idx];
-          frame.parents[idx] = frame.parents[frame.offset];
-          frame.parents[frame.offset] = tmp;
-          frame.offset++;
-        }
-        // else seen this parent at previous point and so don't increment offset
-      }
-    }
+  if (child === null) return;
+
+  const frame = parentsStack[parentsStack.length - 1];
+  const idx = frame.parents.indexOf(p);
+
+  if (idx === frame.offset) {
+    // nothing to do, just skip over
+    frame.offset++;
+    return;
   }
+  // look for this parent elsewhere
+  if (idx === -1) {
+    // not seen this parent yet, add it in the correct place
+    // and push the one currently there to the end (likely that we'll be
+    // getting rid of it)
+    // sneaky hack for doing captureDereferences
+    if (child !== void 0) {
+      addToArray(p._activeChildren, child);
+    }
+    frame.parents.splice(frame.offset, 0, p);
+    frame.offset++;
+    return;
+  }
+  if (idx > frame.offset) {
+    // seen this parent after current point in array, so swap positions
+    // with current point's parent
+    const tmp = frame.parents[idx];
+    frame.parents[idx] = frame.parents[frame.offset];
+    frame.parents[frame.offset] = tmp;
+    frame.offset++;
+  }
+  // else seen this parent at previous point and so don't increment offset
 }
 
 export function captureDereferences(f) {
